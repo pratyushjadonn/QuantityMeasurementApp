b863c6f5ebd189a094dc77b3b538d1bced288733
pratyushjadonn <pratyush.jadon_cs22@gla.ac.in>
2026-02-22 00:49:24 +0530

    [Pratyush]add. Implement feet measurement equality

---
+++ src/test/java/com/example/QuantityMeasurmentApp/QuantityMeasurmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuantityMeasurmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityMeasurmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
