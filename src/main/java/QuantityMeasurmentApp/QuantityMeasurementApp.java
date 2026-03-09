15be5ae30dce10b26c0d21233f0e60e82cd07e70
pratyushjadonn <pratyush.jadon_cs22@gla.ac.in>
2026-03-09 12:08:04 +0530

    [Pratyush].added UC7: Added equality support with unit conversion to common target unit

--- src/main/java/QuantityMeasurmentApp/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurmentApp/QuantityMeasurementApp.java
@@ -2,40 +2,39 @@ package QuantityMeasurmentApp;
 import java.util.*;
 public class QuantityMeasurementApp {
 	//generic method to demonstrate Length equality check
-	public static boolean demonstrateLengthEquality(Length length1, Length length2) {
-		if (length1 == null || length2 == null) {
-			return false;
+		public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+			if (length1 == null || length2 == null) {
+				return false;
+			}
+
+			boolean result = length1.equals(length2);
+			System.out.println("Comparing: " + length1 + " and " + length2);
+			System.out.println("Are equal? " + result);
+			return result;
+		}
+		//overloaded method to accept raw values and units
+		public static boolean demonstrateLengthComparison(double value1, Length.LengthUnit unit1, double value2,
+				Length.LengthUnit unit2) {
+
+			Length length1 = new Length(value1, unit1);
+			Length length2 = new Length(value2, unit2);
+
+			return demonstrateLengthEquality(length1, length2);
 		}
 
-		boolean result = length1.equals(length2);
-		System.out.println("Comparing: " + length1 + " and " + length2);
-		System.out.println("Are equal? " + result);
-		return result;
-	}
-	//overloaded method to accept raw values and units
-	public static boolean demonstrateLengthComparison(double value1, Length.LengthUnit unit1, double value2,
-			Length.LengthUnit unit2) {
-
-		Length length1 = new Length(value1, unit1);
-		Length length2 = new Length(value2, unit2);
-
-		return demonstrateLengthEquality(length1, length2);
-	}
 
 
+		public static void main(String[] args) {
+		    Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+		    Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
 
-	public static void main(String[] args) {
+		    Length resultFeet = l1.add(l2, Length.LengthUnit.FEET);
+		    Length resultInches = l1.add(l2, Length.LengthUnit.INCHES);
+		    Length resultYards = l1.add(l2, Length.LengthUnit.YARDS);
 
-        Length l1 =
-                new Length(1.0,
-                        Length.LengthUnit.FEET);
+		    System.out.println(resultFeet); 
+		    System.out.println(resultInches); 
+		    System.out.println(resultYards); 
 
-        Length l2 =
-                new Length(12.0,
-                        Length.LengthUnit.INCHES);
-
-        Length result = l1.add(l2);
-
-        System.out.println(result);
-    }
+	    }
 }
